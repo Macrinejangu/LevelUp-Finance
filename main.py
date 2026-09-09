2138c4001397b4d28178c694f3a68fda686e3e88
Enoch Mwenda <enochmungiria@gmail.com>
2026-09-09 20:07:17 +0300

    updated changes in database and account pages

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ Entry point for LevelUp Finance.
 → Run with: python main.py
 """
 from levelup.cli import main
+from levelup.database import init_db
+
+
 
 if __name__ == "__main__":
-    main()
+  init_db()
+  main()
+    
